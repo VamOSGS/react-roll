7de614aab96b08f6c1269ba1c7aadb06758c9a3a
VamOSGS <vamosgs2001@gmail.com>
2018-01-29 23:01:23 +0400

    Controll added

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
+import Roll from './Roll';
+import { FormControl, FormHelperText } from 'material-ui/Form';
+import Select from 'material-ui/Select';
+import Button from 'material-ui/Button';
+import Input, { InputLabel } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
 
-class App extends Component {
+export default class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            age: 5,
+            name: 'nice'
+        };
     }
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
     render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <div className="controll">
+                    <form autoComplete="off">
+                        <FormControl>
+                            <InputLabel>Number</InputLabel>
+                            <Select
+                                value={this.state.age}
+                                onChange={this.handleChange}
+                                inputProps={{
+                                    name: 'age',
+                                    id: 'age-simple'
+                                }}
+                            >
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </form>
+                    <Button raised>Roll</Button>
+                </div>
+                <Roll />
             </div>
         );
     }
 }
-
-export default App;
---
+++ src/js/components/Roll.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Roll extends Component {
+    render() {
+        return (
+            <div className={'Roll'}>
+                <h1>Roll</h1>
+            </div>
+        );
+    }
+}
+ 
